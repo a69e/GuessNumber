4f91a497156a3bbe50174575eba8bbd314334779
joeseee <joesee@qq.com>
2018-11-30 15:28:12 -0500

    Update my_web.py
    add start button and related logic

--- my_web.py
+++ my_web.py
@@ -2,28 +2,48 @@ from flask import Flask, request, render_template
 from guess_number import gen_number, hint_for_web
 
 app = Flask(__name__)
+# TODO: need to move this part to session to support multiple users
 message_history = []  # To gather all messages received.
-number = gen_number()  # To generate a random 4 digit numbers.
+number = None  # To generate a random 4 digit numbers.
+username = ""
 
 
 # To log all messages and return with one single string.
 def log(message):
+    global message_history
     message_history.append(message)
     message_history_in_one_string = "<br />".join(message_history)
     return message_history_in_one_string
 
 
+@app.route('/gameStart')
+def game_start():
+    global number, message_history, username
+    username = request.args.get('username', '')
+    if username != '':
+        number = gen_number()
+        message_history = []
+        print number
+        return render_template('my_web.html', hint=log("Let's play a game!"), username=username)
+    else:
+        return render_template('my_web.html', hint=log("username is blank"), username=username)
+
+
 @app.route('/')
 def hello():
+    global number, message_history, username
     guess = request.args.get('number', '')
-    if guess == '':
-        return render_template('my_web.html', hint=log("Let's play a game!"))
-    elif not guess.isdigit():
-        return render_template('my_web.html', hint=log('Please enter numbers only!'))
-    elif len(list(guess)) != 4:
-        return render_template('my_web.html', hint=log('Please enter 4 numbers exactly!'))
-    else:
-        return render_template('my_web.html', hint=log(hint_for_web(number, guess)))
+    if number is None:
+        return render_template('my_web.html', hint=log("the game is not started"), username=username)
+    if guess == '' or not guess.isdigit():
+        return render_template('my_web.html', hint=log('Please enter numbers only!'), username=username)
+    if len(list(guess)) != 4:
+        return render_template('my_web.html', hint=log('Please enter 4 numbers exactly!'), username=username)
+
+    result_str = hint_for_web(number, guess)
+    if "Game ended." in result_str:
+        number = None
+    return render_template('my_web.html', hint=log(result_str), username=username)
 
 
 if __name__ == '__main__':
