fe7d43cfd5779240f457884911ba89883a2174cf
a69e <outlaw.lawout@gmail.com>
2018-12-20 16:22:43 -0500

    Completed version 1.0!!! Good job!

--- guess_number_poor_ai.py
+++ guess_number_poor_ai.py
@@ -2,11 +2,10 @@ from random import sample
 from itertools import permutations
 import time
 
-
 number = []  # The random number that need to guess.
 ten_numbers = '0123456789'
 info = {}  # Stores all guesses and their hints along the game.
-counter = 1  # To count the steps.
+counter = 0  # To count the steps.
 success = 0  # To count the successful game for multiple game plays, and display the outcome.
 
 
@@ -44,17 +43,17 @@ def get_number_left_unpicked_from_a_guess(guess, picked):
 # To get the hint from any guess.
 def hints(guess):
     global counter, success
+    counter += 1
     hint = [0, 0, counter]  # 1st: A, 2nd: B, 3rd: step.
     for i in range(0, 4):
         if guess[i] == number[i]:
             hint[0] += 1
         elif guess[i] in number:
             hint[1] += 1
-    counter += 1
     if hint[0] == 4:
         success += 1
         print 'Game completed!'
-        print 'Steps: ' + str(counter - 1)
+        print 'Steps: ' + str(counter)
     info[guess] = hint
     return guess, hint
 
@@ -74,15 +73,48 @@ def let_3rd_be_4th(guess):
     return guess[0][0:2] + guess[0][3] + guess[0][2]
 
 
-# To pick the guess in the remaining possible set until reach 4A.
-def guess_until_4A(remaining):
+# This handles those guesses which contains all 4 correct numbers but you may still need to guess 2 or more times.
+def from_4B_to_4A(guess):
+    # To unify the format of guess.
+    if len(guess) == 2:
+        guess = guess[0]
+
+    # To make sure not to guess the same number a second time.
+    remaining = next_best_guess(guess, 'list')
+    count = 0
     for i in remaining:
-        if hints(next_best_guess(''.join(i)))[1][0] == 4:
-            break
+        for j in info:
+            for k in range(0, 4):
+                if i[k] == j[k]:
+                    count += 1
+            if count == 4:
+                remaining.remove(i)
+            count = 0
+
+    # The reason here we make another guess and update the remaining list accordingly b4 using the loop below,
+    # is to make sure to avoid duplicate/multiple guesses which are unnecessary.
+    if hints(remaining[0])[1][0] != 4:
+        # To make sure once again not to guess the same number a second time.
+        remaining = next_best_guess(guess, 'list')
+        count = 0
+        for i in remaining:
+            for j in info:
+                for k in range(0, 4):
+                    if i[k] == j[k]:
+                        count += 1
+                if count == 4:
+                    remaining.remove(i)
+                count = 0
+
+        # To pick the guess in the remaining possible set until reach 4A.
+        for i in remaining:
+            if hints(''.join(i))[1][0] == 4:
+                break
 
 
-# Below 3 functions (filter_group110(), filter_group200() and from_no_info_4B_to_4A()) are written at the beginning,
-# and it only solves one particular case , so that the logic is somehow different from other functions.
+# Below 4 functions (filter_group110(), filter_group200(), guess_until_4A() and from_no_info_4B_to_4A())
+# only solves one particular case , so that the logic is somehow different from other functions.
+# This is the earliest written function, so it is a bit out of order and of low efficiency. :)
 # TODO: Can be integrated into the same other logic (e.g. next_best_guess()) later on.
 
 # Only used by from_no_info_4B_to_4A(), to filter out invalid numbers for below groups: 01, 10
@@ -164,6 +196,13 @@ def filter_group200(guess1, guess2):
     return full_list
 
 
+def guess_until_4A(remaining):
+    next_best_guess(remaining[0], 'string', remaining)
+    for i in remaining:
+        if hints(''.join(i))[1][0] == 4:
+            break
+
+
 # "no_info_4B" here means there's no previous guess, all information we have is this guess of 4B.
 def from_no_info_4B_to_4A():
     # First, find the guess that contains all 4 correct numbers, assigned to guess1.
@@ -208,18 +247,23 @@ def from_no_info_4B_to_4A():
 # If after applying a rule will return an empty list, then it will roll back and skip applying that rule.
 # This is just to make sure that the return is always non-empty, so as to carry on the remaining steps.
 # TODO: Having said that, the game steps will be smaller if we can figure a way to improve the logic.
-def next_best_guess(guess, return_type='string'):
+def next_best_guess(guess, return_type='string', remaining=None):
     to_be_deleted = []  # To store all invalid numbers and then delete in one go.
     possible_list = [guess, ]  # To store all possible combinations after filtering some rules, only if it is non-empty.
     full_list = list(permutations([guess[i] for i in range(0, 4)]))  # All possible combinations.
     count = 0
+
+    # We've added a new handling here especially for from_no_info_4B_to_4A().
+    if remaining is not None:
+        possible_list = remaining
+        full_list = remaining
 
     # for testing purpose:
     # print 'next_best_guess:all '
     # print full_list
     # print possible_list
 
-    # Applying rules: '0A'+'1A0B'+'1AyB'.
+    # Applying rules below 1 by 1: '0A'+'1A0B'+'1AyB'+'2AyB'+'3AyB'.
 
     # To utilize '0A' to exclude invalid numbers ('0A' means the correct numbers did not appear at the correct position)
     for i in guess:
@@ -237,11 +281,6 @@ def next_best_guess(guess, return_type='string'):
     else:
         full_list = list(possible_list)  # Roll back full_list to previous non-empty set to avoid it becomes empty.
 
-    # for testing purpose:
-    print 'next_best_guess:0A'
-    print full_list
-    # print possible_list
-
     # To utilize '1A0B' to exclude invalid numbers ('1A0B' means the correct number is in the correct positions).
     for i in guess:
         for j in info:
@@ -257,11 +296,6 @@ def next_best_guess(guess, return_type='string'):
         possible_list = tuple(full_list)  # It will update(truncate) possible_list if full_list is not empty.
     else:
         full_list = list(possible_list)  # Roll back full_list to previous non-empty set to avoid it becomes empty.
-
-    # for testing purpose:
-    print 'next_best_guess:1A0B'
-    print full_list
-    # print possible_list
 
     # To utilize '1AyB'(y>=1) to exclude invalid numbers ('1AyB' means not all the numbers are in correct positions).
     for i in info:
@@ -281,35 +315,41 @@ def next_best_guess(guess, return_type='string'):
     else:
         full_list = list(possible_list)  # Roll back full_list to previous non-empty set to avoid it becomes empty.
 
-    # for testing purpose:
-    print 'next_best_guess:1AyB'
-    print full_list
-    print possible_list
+    # To utilize '2AyB' to exclude invalid numbers.
+    # '2AyB' means there are 2 and only 2 correct numbers are in the correct positions.
+    for i in info:
+        if info[i][0] == 2:  # This if means the number matches with '2AyB'
+            for j in full_list:
+                for k in range(0, 4):
+                    if i[k] == j[k]:
+                        count += 1
+                if count != 2:  # This measures how many numbers the input in full_list matches with the guess in info.
+                    to_be_deleted.append(j)
+                count = 0
+            for l in to_be_deleted:
+                full_list.remove(l)
+            to_be_deleted = []
+    if full_list:
+        possible_list = tuple(full_list)  # It will update(truncate) possible_list if full_list is not empty.
+    else:
+        full_list = list(possible_list)  # Roll back full_list to previous non-empty set to avoid it becomes empty.
 
-    # To utilize '2A0B' to exclude invalid numbers ('2A0B' means the 2 correct numbers are in the correct positions).
-
-    # for testing purpose:
-    # print 'next_best_guess:2A0B '
-    # print full_list
-    # print possible_list
-
-    # This loop try to form a better guess such that each of the number had never been guessed in that position.
-    # TODO: This part is very unlikely to be used anymore, but just keep it here for safe.
-    '''for i in guess:
-        for j in info:
-            if i in j:
-                index = list(j).index(i)
-                for k in full_list:
-                    if k[index] == i:
-                        to_be_deleted.append(k)
-                if len(full_list) != len(to_be_deleted):
-                    for l in to_be_deleted:
-                        full_list.remove(l)
-                to_be_deleted = []
-
-
-        print 'next_best_guess:new position '
-        print full_list'''
+    # To utilize '3AyB' to exclude invalid numbers.
+    # '3AyB' means there are 3 and only 3 correct numbers are in the correct positions.
+    for i in info:
+        if info[i][0] == 3:  # This if means the number matches with '3AyB'
+            for j in full_list:
+                for k in range(0, 4):
+                    if i[k] == j[k]:
+                        count += 1
+                if count != 3:  # This measures how many numbers the input in full_list matches with the guess in info.
+                    to_be_deleted.append(j)
+                count = 0
+            for l in to_be_deleted:
+                full_list.remove(l)
+            to_be_deleted = []
+    if not full_list:
+        full_list = list(possible_list)  # Roll back full_list to previous non-empty set to avoid it becomes empty.
 
     # Please note that the list can contain more than 1 guess. In fact, all of them will the same new information.
     if return_type is 'list':  # Return the full available list if return_type is 'list'.
@@ -322,29 +362,6 @@ def next_best_guess(guess, return_type='string'):
         return ''.join(full_list[0])
 
 
-# This handles those guesses which contains all 4 correct numbers but you may still need to guess 2 or more times.
-def from_4B_to_4A(guess):  # guess[0][0], guess[0][1], guess[0][2], guess[0][3]
-    # To unify the format of guess.
-    if len(guess) == 2:
-        guess = guess[0]
-
-    # To make sure not to guess the same number a second time.
-    remaining = next_best_guess(guess, 'list')
-    count = 0
-    for i in remaining:
-        for j in info:
-            for k in range(0, 4):
-                if i[k] == j[k]:
-                    count += 1
-            if count == 4:
-                remaining.remove(i)
-            count = 0
-
-    # To get all possible combinations and guess until reach 4A.
-    guess_until_4A(remaining)
-
-
-# TODO: need to enhance the logic (also need to enhance pick_until_4A()).
 def from_2B_to_4A(guess1, guess2):
     guess3 = hints(guess1[0][1] + guess2[0][0:3])
 
@@ -362,7 +379,7 @@ def from_2B_to_4A(guess1, guess2):
                 from_4B_to_4A(guess1[0][0] + guess3[0][0:3])
             if AplusB(guess5) == 2:
                 guess6 = hints(next_best_guess(guess1[0][1:3] + guess3[0][2:4]))
-                if AplusB(guess6) == 4:
+                if AplusB(guess6) == 4 and guess6[1][0] != 4:
                     from_4B_to_4A(guess6)
                 if AplusB(guess6) == 3:
                     from_4B_to_4A(guess1[0][1:3] + guess3[0][1] + guess3[0][3])
@@ -564,27 +581,19 @@ def from_definite_2B_and_1_to_4A(guess1, guess2):
     # Because there are only 2 correct numbers from first 2 guesses, so the 2 un-guessed numbers must be correct.
     two_definite_correct_numbers = pick_x_number_from_not_used(2)
 
-    # two_numbers and other_two_numbers are the 4 numbers that form guess1.
-    two_numbers_in_guess1 = pick_number_from_a_string(guess1[0], 2)
-    other_two_numbers_in_guess1 = get_number_left_unpicked_from_a_guess(guess1[0], two_numbers_in_guess1)
-
-    # two_numbers and other_two_numbers are the 4 numbers that form guess2.
-    two_numbers_in_guess2 = pick_number_from_a_string(guess2[0], 2)
-    other_two_numbers_in_guess2 = get_number_left_unpicked_from_a_guess(guess2[0], two_numbers_in_guess2)
-
     # These 2 guesses are trying to find the correct numbers from which half.
     # TODO: think about how to use next_best_guess().
-    guess_1 = hints(two_definite_correct_numbers + two_numbers_in_guess1)
-    guess_2 = hints(two_definite_correct_numbers + two_numbers_in_guess2)
+    guess_1 = hints(two_definite_correct_numbers + guess1[0][0:2])
+    guess_2 = hints(guess2[0][2:4] + two_definite_correct_numbers)
 
     if AplusB(guess_1) == 3 and AplusB(guess_2) == 3:
-        two_other_correct_numbers_are_in_this_guess = two_numbers_in_guess1 + two_numbers_in_guess2
+        two_other_correct_numbers_are_in_this_guess = guess1[0][0:2] + guess2[0][2:4]
     if AplusB(guess_1) == 3 and AplusB(guess_2) == 2:
-        two_other_correct_numbers_are_in_this_guess = two_numbers_in_guess1 + other_two_numbers_in_guess2
+        two_other_correct_numbers_are_in_this_guess = guess1[0][0:2] + guess2[0][0:2]
     if AplusB(guess_1) == 2 and AplusB(guess_2) == 3:
-        two_other_correct_numbers_are_in_this_guess = other_two_numbers_in_guess1 + two_numbers_in_guess2
+        two_other_correct_numbers_are_in_this_guess = guess1[0][2:4] + guess2[0][2:4]
     if AplusB(guess_1) == 2 and AplusB(guess_2) == 2:
-        two_other_correct_numbers_are_in_this_guess = other_two_numbers_in_guess1 + other_two_numbers_in_guess2
+        two_other_correct_numbers_are_in_this_guess = guess1[0][2:4] + guess2[0][0:2]
 
     from_definite_2B_and_2_to_4A((two_other_correct_numbers_are_in_this_guess, [0, 0, 0]), two_definite_correct_numbers)
 
@@ -768,18 +777,18 @@ def from_definite_3B_to_4A(three, guess):
 
 def play():
     # Based on the first 1~2 guesses, all 12 groups are: 02, 03, 04, 11, 12, 13, 20, 21, 22, 30, 31, 4
-    # guess1 = hints(gen_number(4))
+    guess1 = hints(gen_number(4))
     # For testing purpose:
-    guess1 = hints('1234')
+    # guess1 = hints('1234')
 
     # Similar algorithm for group 4 & 04 ---------------------------------------
     if AplusB(guess1) == 4:
         if guess1[1][0] != 4:
             from_no_info_4B_to_4A()
     else:
-        # guess2 = hints(pick_x_number_from_not_used(4))
+        guess2 = hints(pick_x_number_from_not_used(4))
         # For testing purpose:
-        guess2 = hints('5678')
+        # guess2 = hints('5678')
 
         if AplusB(guess1) == 0 and AplusB(guess2) == 4:
             from_no_info_4B_to_4A()
@@ -833,64 +842,73 @@ def play():
             from_2B_to_4A(guess1, guess2)
 
 
+# This is the demo to show how a normal game processes.
 def play_one_game():
     global number
     number = gen_number(4)
-    # number = '5906'
-    print number
+    print 'Target number: ' + str(number)
     play()
-    print info
+    print 'Xth:   guess:  hint:'
+    for i in range(1, 12):
+        for j in info:
+            if info[j][2] == i:
+                print str(i) + '      ' + str(j) + '    ' + str(info[j][0]) + 'A' + str(info[j][1]) + 'B'
 
 
-# Good for testing.
+# This is for testing, the loop will break if condition set satisfied.
 def play_one_hundred_game():
     # Timer.
     start_time = time.time()
 
     global number, success, ten_numbers, info, counter
     # The 0th element is total steps, the ith element is the count of games that completes within that step.
-    steps = [0*i for i in range(0, 30)]
-    for i in range(0, 50):
+    steps = [0 * i for i in range(0, 15)]
+    for i in range(0, 100):
         number = gen_number(4)
         print number
         play()
         print info
 
         # Do statistics.
-        steps[0] += counter
-        for j in range(1, 30):
+        steps[0] += counter - 1
+        for j in range(1, 15):
             if counter == j:
                 steps[j] += 1
+
+        # For testing purpose.
+        if counter >= 100:
+            break
 
         # Reset variables after each game.
         ten_numbers = '0123456789'
         info = {}
-        counter = 1
+        counter = 0
 
     # Timer.
     end_time = time.time()
 
     # Output.
     print 'Statistics:'
-    print 'Game played: 50'
+    print 'Game played: 100'
     print 'Successes: ' + str(success)
-    print 'Failures: ' + str(50-success)
+    print 'Failures: ' + str(100 - success)
     print 'Time used: ' + str(round(end_time - start_time, 2)) + ' seconds'
     print 'Total steps: ' + str(steps[0])
-    print 'Average steps: ' + str(round(float(steps[0])/float(50), 2))
+    print 'Average steps: ' + str(round(float(steps[0]) / float(100), 2))
     print 'How many games completed with that steps:'
-    for i in range(1, 30):
+    for i in range(1, 15):
         print str(i) + ' steps: ' + str(steps[i])
 
 
-def play_ten_thousands_game():
+# This is for statistics.
+def play_one_hundred_thousand_game():
     # Timer.
     start_time = time.time()
 
     global number, success, ten_numbers, info, counter
     # The 0th element is total steps, the ith element is the count of games that completes within that step.
-    steps = [0*i for i in range(0, 30)]
-    for i in range(0, 10000):
+    steps = [0 * i for i in range(0, 15)]
+    for i in range(0, 100000):
         number = gen_number(4)
         print number
         play()
@@ -898,32 +916,32 @@ def play_ten_thousands_game():
 
         # Do statistics.
         steps[0] += counter
-        for j in range(1, 30):
+        for j in range(1, 15):
             if counter == j:
                 steps[j] += 1
 
         # Reset variables after each game.
         ten_numbers = '0123456789'
         info = {}
-        counter = 1
+        counter = 0
 
     # Timer.
     end_time = time.time()
 
     # Output.
     print 'Statistics:'
-    print 'Game played: 10000'
+    print 'Game played: 100000'
     print 'Successes: ' + str(success)
-    print 'Failures: ' + str(10000-success)
+    print 'Failures: ' + str(100000 - success)
     print 'Time used: ' + str(round(end_time - start_time, 2)) + ' seconds'
     print 'Total steps: ' + str(steps[0])
-    print 'Average steps: ' + str(round(float(steps[0])/float(10000), 2))
+    print 'Average steps: ' + str(round(float(steps[0]) / float(100000), 2))
     print 'How many games completed with that steps:'
-    for i in range(1, 30):
+    for i in range(1, 15):
         print str(i) + ' steps: ' + str(steps[i])
 
 
 if __name__ == '__main__':
-    # play_one_game()
+    play_one_game()
     # play_one_hundred_game()
-    play_ten_thousands_game()
+    # play_one_hundred_thousand_game()
